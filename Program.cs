1f951d0ebb8fb7058d12aad1ca23ec2d72eb769b
BlagoKolev <blagovest.kolev@gmail.com>
2024-03-09 00:03:29 +0200

    Update if statement. Create a Swap function.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@
         {
             var arr = new int[12] { 7, 5, 1, 3, 2, 2, 6, 4, 9, 8, 10, 11 };
             var result = SelectionSort(arr);
+            
+            static int[] Swap(int[] array, int currentElement, int currentElementIndex, int otherElement, int otherElementIndex)
+            {
+                var tempElement = currentElement;
+                array[currentElementIndex] = otherElement;
+                array[otherElementIndex] = tempElement;
+                return array;
+            }
 
             #region [Selection-Sort]
+            //Selection Sort   – O(n2) / unstable / selection; Simple but not efficient. Swap the first element with the min element to the right. Then the second element, etc.
             static int[] SelectionSort(int[] arr)
             {
                 for (int i = 0; i < arr.Length - 1; i++)
@@ -20,8 +29,8 @@
                     {
                         var nextElement = arr[j];
                         if (nextElement < currentElement)
-                        {
-                            if (nextElement < minElement)
+                        { //Here we check not only for lower then, but and for equals becasuse the algo should be unstable
+                            if (nextElement <= minElement)
                             {
                                 minElement = nextElement;
                                 minElementIndex = j;
@@ -30,9 +39,10 @@
                     }
                     if (minElement < currentElement)
                     {
-                        var tempElement = currentElement;
-                        arr[i] = minElement;
-                        arr[minElementIndex] = tempElement;
+                        //var tempElement = currentElement;
+                        //arr[i] = minElement;
+                        //arr[minElementIndex] = tempElement;
+                        Swap(arr, currentElement, i, minElement, minElementIndex);
                     }
                 }
 
